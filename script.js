cacd46ffcecac864a557700a68ca74a18fd4924a
Shubhankar Sharma <shubhankar35sharma@gmail.com>
2022-08-17 22:46:27 +0530

    third
    third

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  // .to(".back-vid", 10, { y: -300 })
-  // // .to(".back-vid", 10, { y: -200 }, "-=10")
   .fromTo(".back-vid", { y: -30 }, { x: 0, duration: 10 }, "-=8")
   .fromTo(".logocontent", { x: -1400 }, { x: 0, duration: 6 }, "-=4")
   .to(".content1", 10, { top: "0%" }, "-=7")
