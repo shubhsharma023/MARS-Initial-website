b168c8fad75d47d7339ebd17e960f71e7b4b25ca
Shubhankar Sharma <shubhankar35sharma@gmail.com>
2022-08-30 23:39:15 +0530

    dsfsd
    sdfsd

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
+  // .to(".back-vid", 10, { y: -300 })
+  // // .to(".back-vid", 10, { y: -200 }, "-=10")
   .fromTo(".back-vid", { y: -30 }, { x: 0, duration: 10 }, "-=8")
-  .fromTo(".logocontent", { x: -1400 }, { x: 200, duration: 4 }, "-=1")
-  .fromTo(".tagline", { opacity: 0 }, { opacity: 1, duration: 2 })
+  .fromTo(".logocontent", { x: -1400 }, { x: 200, duration: 6 }, "-=4")
   .to(".content1", 10, { top: "0%" }, "-=7")
   .fromTo(".content1-images", { opacity: 0 }, { opacity: 1, duration: 4 })
   .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 4 })
