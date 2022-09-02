cc7c6b18b8abe8cb4d3eeb6412e273c90a7ff93d
Shubhankar Sharma <shubhankar35sharma@gmail.com>
2022-09-02 13:41:00 +0530

    update
    update

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .fromTo(".back-vid", { y: -30 }, { x: 0, duration: 10 }, "-=8")
-  .fromTo(".logocontent", { x: -1400 }, { x: 200, duration: 4 }, "-=1")
+  .fromTo(".back-vid", { y: 0 }, { x: 0, duration: 10 }, "-=4")
+  .fromTo(".logocontent", { x: -1400 }, { x: 40, duration: 4 }, "-=1")
   .fromTo(".tagline", { opacity: 0 }, { opacity: 1, duration: 2 })
   .to(".content1", 10, { top: "0%" }, "-=7")
   .fromTo(".content1-images", { opacity: 0 }, { opacity: 1, duration: 4 })
